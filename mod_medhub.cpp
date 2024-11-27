28525f8e7e965ba39de6257827d2865922eb3135
maming <ming.ma@yulore.com>
2024-11-27 11:16:39 +0800

    test: switch_ivr_broadcast_in_thread, add log

--- mod_medhub.cpp
+++ mod_medhub.cpp
@@ -2033,7 +2033,11 @@ SWITCH_STANDARD_API(hub_uuid_play_function) {
                 switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "[%s] hub_uuid_play: %s\n",
                                   ctx->sessionid, filename);
                 // switch_ivr_broadcast(argv[0], filename, (SMF_NONE | SMF_ECHO_ALEG | SMF_ECHO_BLEG));
+                switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "hub_uuid_play: %s before switch_ivr_broadcast_in_thread: %s\n",
+                                  switch_core_session_get_uuid(session4play), filename);
                 switch_ivr_broadcast_in_thread(session4play, filename, (SMF_NONE | SMF_ECHO_ALEG | SMF_ECHO_BLEG));
+                switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "hub_uuid_play: %s after switch_ivr_broadcast_in_thread: %s\n",
+                                  switch_core_session_get_uuid(session4play), filename);
             }
         }
     }
